d5632e0f7b225afbf1ce2e6e17b1812a41a85017
Josh Burgoyne <jburgoyne86@gmail.com>
2024-02-09 20:03:21 -0700

    added js script and canvas loaded

---
+++ play.js
@@ -0,0 +1,34 @@
+
+
+
+var width = window.innerWidth;
+var height = width;
+var blockSize = 25;
+var rows = 20;
+var cols = 20;
+
+var context;
+
+var heroX = 4 * blockSize;
+var heroY = 4 * blockSize;
+
+window.onload = function () {
+    canvas = document.getElementById('canvas');
+    canvas.width = cols * blockSize;
+    canvas.height = rows * blockSize;
+    context = canvas.getContext("2d");
+
+    setInterval(update, 1000/10);
+    
+}
+
+function update() {
+    context.fillStyle = "black";
+    context.fillRect(0, 0, cols * blockSize, rows * blockSize);
+
+    context.fillStyle = "aquamarine";
+    context.fillRect(heroX, heroY, blockSize, blockSize);
+}
+
+
+
