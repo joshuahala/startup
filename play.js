2f1de1522d33ffec2ec994f83dcd6d40678bf858
Josh Burgoyne <jburgoyne86@gmail.com>
2024-02-19 18:15:15 -0700

    added start text and enter to start

--- play.js
+++ play.js
@@ -12,7 +12,7 @@ var cols = 20;
 
 var score = 0;
 let lives = 3;
-let spawnHazards = true;
+let spawnHazards = false;
 
 var context;
 
@@ -23,6 +23,8 @@ var goalX = 0 * blockSize;
 var goalY = 0 * blockSize;
 
 let hazards = [];
+
+const startText = document.getElementById('start-text');
 
 class Hazard {
     constructor(hx, hy, dir) {
@@ -61,11 +63,13 @@ window.onload = function () {
     //spawnHazard();
 
     document.addEventListener("keyup", (e) => {
-        if (e.key == " ") {
+        if (e.key == 'Enter') {
             spawnHazards = !spawnHazards;
+            spawnHazards == true ? document.getElementById('start-text').style.display = "none" : document.getElementById('start-text').style.display = "block";
             lives = 3;
             score = 0;
         }
+        console.log(e.key);
     });
 
     document.addEventListener("keydown", arrowMove);
@@ -102,11 +106,12 @@ function update() {
         //check for collision
         if(heroX == hazard.hx && heroY == hazard.hy) {
             spawnHazards = false;
+            document.getElementById('start-text').style.display = "block";
         }
     }
 
     //check if reached goal
-    if (goalX == heroX && goalY == heroY) {
+    if (goalX == heroX && goalY == heroY && spawnHazards == true) {
         score ++;
         setGoal();
     }
@@ -167,21 +172,24 @@ function spawnHazard() {
 
 function arrowMove(e) {
     
+    
     if(["ArrowUp", "ArrowDown", "ArrowLeft", "ArrowRight", "w", "s", "a", "d"].includes(e.key)) {
         e.preventDefault();
     }
-    
-    if ((e.key == "ArrowUp" || e.key == "w") && heroY != 0) {
-        heroY -= 1 * blockSize;
-    }
-    else if ((e.key == "ArrowDown" || e.key == "s") && heroY != 19*blockSize) {
-        heroY += 1 * blockSize;
-    }
-    else if ((e.key == "ArrowLeft" || e.key == "a") && heroX != 0) {
-        heroX -= 1 * blockSize;
-    }
-    else if ((e.key == "ArrowRight" || e.key == "d") && heroX != 19*blockSize) {
-        heroX += 1 * blockSize;
+    if(spawnHazards == true) {
+        
+        if ((e.key == "ArrowUp" || e.key == "w") && heroY != 0) {
+            heroY -= 1 * blockSize;
+        }
+        else if ((e.key == "ArrowDown" || e.key == "s") && heroY != 19*blockSize) {
+            heroY += 1 * blockSize;
+        }
+        else if ((e.key == "ArrowLeft" || e.key == "a") && heroX != 0) {
+            heroX -= 1 * blockSize;
+        }
+        else if ((e.key == "ArrowRight" || e.key == "d") && heroX != 19*blockSize) {
+            heroX += 1 * blockSize;
+        }
     }
 }
 
@@ -215,5 +223,10 @@ function toggleMenu() {
         console.log("closed");
 
     }
+}
+
+function startGame() {
+    spawnHazards = true;
+    document.getElementById('start-text').style.display = "none";
 }
 
