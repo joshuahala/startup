fc045e919522f12bf5d084273af8ae87cc64bfcb
Josh Burgoyne <jburgoyne86@gmail.com>
2024-02-22 15:43:32 -0700

    heroObj class for adding hero cards

--- heroes.js
+++ heroes.js
@@ -1,13 +1,53 @@
 
+class Hero {
+    constructor(name, color, level){
+        this.name = name;
+        this.color = color;
+        this.level = level;
+    }
 
-function addHero() {
+    url(color) {
+        switch(color) {
+            case "blue":
+                return 'assets/images/blue-only.png';
+                
+            case "red":
+                return 'assets/images/red-only.png';
+                
+        }
+    }
+}
+
+blueHero = new Hero("freeze", "blue", 1);
+
+function addHero(heroObj) {
     alert("sdfsf")
     let heroes = document.getElementById('heroes-container');
-    hero = document.createElement('div');
-    hero.classList.add('hero-card');
-    cardBottom = document.createElement('div');
-    cardBottom.classList.add('card-bottom');
 
-    hero.appendChild(cardBottom);
-    heroes.appendChild(hero);
+    let heroEl = document.createElement('div');//create new hero div
+    heroEl.classList.add('hero-card');// add hero class
+    cardBottom = document.createElement('div');// add bottom label
+    cardBottom.classList.add('card-bottom');//add class to bottom label
+    //add the level and train button to the card
+    let level = document.createElement('p');
+    level.textContent = `LVL ${heroObj.level}`;
+    let linkP = document.createElement('p');
+    let linkA = document.createElement('a');
+    
+    linkA.href = "play.html";
+    linkA.textContent = "TRAIN";
+
+    linkP.appendChild(linkA);
+    cardBottom.appendChild(level);
+    cardBottom.appendChild(linkP);
+
+    heroEl.appendChild(cardBottom);// add bottom label to hero card
+    heroes.appendChild(heroEl);// add hero card to the heroes container
+
+    //add the image to the hero background
+    heroEl.style.backgroundImage = `url(${heroObj.url(heroObj.color)}`;
+}
+
+function clickAdd() {
+    addHero(blueHero);
 }
