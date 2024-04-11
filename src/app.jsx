bb303c1030c0fe77c100d78685bf061ca1efa4ec
Josh Burgoyne <jburgoyne86@gmail.com>
2024-04-11 17:23:18 -0600

    added router

--- src/app.jsx
+++ src/app.jsx
@@ -1,24 +1,30 @@
 import React from 'react';
 import './app.css';
+import { BrowserRouter, NavLink, Route, Routes } from 'react-router-dom';
+import { Login } from './login/login';
+import { Play } from './play/play';
+import { Scores } from './scores/scores';
+import { Heroes } from './heroes/heroes';
 
 export default function App() {
     return(
+        <BrowserRouter>
          <div className='body'>
              <nav>
                  <div id="nav-left">
                      <h1>bit hero</h1>
-                     <a href="heroes.html">My Heroes</a>
-                     <a href="play.html">Play</a>
-                     <a href="scores.html">Scores</a>
+                     <NavLink to="heroes">My Heroes</NavLink>
+                     <NavLink to="play">Play</NavLink>
+                     <NavLink to="scores">Scores</NavLink>
                  </div>
                  <div id="nav-right">
                      <p id="username"></p>
                  </div>
                  <button onclick="toggleMenu()" class="dropdown-btn">Menu</button>
                  <div class="dropdown-content">
-                     <a href="heroes.html">My Heroes</a>
-                     <a href="play.html">Play</a>
-                     <a href="scores.html">Scores</a>
+                     <NavLink to="heroes">My Heroes</NavLink>
+                     <NavLink to="play">Play</NavLink>
+                     <NavLink to="scores">Scores</NavLink>
                  </div>
              </nav>
              <div className='login-card'>
@@ -35,6 +41,16 @@ export default function App() {
              <footer>
                 <a href="https://github.com/joshuahala/startup">My Startup on Github</a>
             </footer>
+
+        <Routes>
+        <Route path='/' element={<Login />} exact />
+        <Route path='/play' element={<Play />} />
+        <Route path='/scores' element={<Scores />} />
+        <Route path='/heroes' element={<Heroes />} />
+        <Route path='*' element={<Login />} />
+        </Routes>
          </div>
+        </BrowserRouter>
+
     );
   }
---
+++ src/heroes/heroes.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export function Heroes() {
+  return (
+    <main className='container-fluid bg-secondary text-center'>
+      <div>heroes displayed here</div>
+    </main>
+  );
+}
