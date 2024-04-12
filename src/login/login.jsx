736b68c99f4903cd14d6c21558855481de67bb46
Josh Burgoyne <jburgoyne86@gmail.com>
2024-04-12 11:24:15 -0600

    login page looks nice

--- src/login/login.jsx
+++ src/login/login.jsx
@@ -2,8 +2,17 @@ import React from 'react';
 
 export function Login() {
   return (
-    <main className='container-fluid bg-secondary text-center'>
-      <div>login displayed here</div>
-    </main>
+        <div className='login-card'>
+          <h1>Bit Hero</h1>
+          <p>Username</p>
+          <input type="text" id="login-username"></input>
+          <p>Password</p>
+          <input type="text" id="login-password"></input>
+          <p class="error" id="error-message">Please try a different username or password</p>
+          <div id="buttons">
+            <button id="create" onclick="create()">Create</button>
+            <button id="login" onclick="login()">Login</button>
+          </div>
+        </div>
   );
 }
